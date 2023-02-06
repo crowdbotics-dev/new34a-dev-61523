3fb8f3e352cc356a83203abfe209097e8e0cce89
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-02-06 16:56:08 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,3 +1,6 @@
+import { TextInput } from "react-native";
+import { Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +11,37 @@ const Untitled4 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><View style={styles.QDGHRtdo}></View><Text style={styles.XejMJcyQ}>Lorem ipsum…</Text><TextInput style={styles.AfreaISJ}></TextInput></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  QDGHRtdo: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  XejMJcyQ: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  VdoDbMgh: {
+    width: 107,
+    height: 62
+  },
+  AfreaISJ: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30
   }
 });
 export default Untitled4;
